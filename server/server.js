972444270079fcff3f6be41e05d2a749c51aea0b
Andrei Scurtu <andrei.scurtu@mindit.io>
2018-08-30 12:07:31 +0300

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -21,6 +22,18 @@ app.get('/todos', (req, res) => {
     Todo.find().then(todos => {
         res.send({ todos });
     }, e => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('ID is not valid');
+    }
+    Todo
+        .findById(id)
+        .then(todo => todo ? res.send({ todo }) : res.status(404).send('Unable to find todo'))
+        .catch(e => res.status(400).send());
+
 });
 
 app.listen(3000, () => console.log('Started on port 3000'));
