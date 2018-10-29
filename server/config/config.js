50d7436da3276feaee0c6079b63a149841147e3b
Andrei Scurtu <andrei.scurtu@mindit.io>
2018-10-29 12:51:11 +0200

    Update config to use jason file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,9 @@
 const env = process.env.NODE_ENV || 'development',
       localhost = 'mongodb://127.0.0.1:27017/';
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = `${localhost}TodoApp`;
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = `${localhost}TodoAppTest`;
+if (env === 'development' || env === 'test') {
+    const config = require('./config.json'),
+          envConfig = config[env];
+    
+    Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
 }
--- server/models/user.js
+++ server/models/user.js
@@ -50,7 +50,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {    
     const user = this,
           access = 'auth',
-          token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+          token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
     user.tokens.push({access, token});
 
@@ -74,7 +74,7 @@ UserSchema.statics.findByToken = function (token) {
     let decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {        
         return Promise.reject(); // return new Promise((resolve, reject) => reject());
     }
